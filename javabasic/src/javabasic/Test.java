6c0c45ec4c00f69da70064557106f7bbdc3be4af
Administrator <Administrator@DESKTOP-HUJUVSS>
2024-10-25 15:09:23 +0900

    10/25

---
+++ javabasic/src/javabasic/Test.java
@@ -0,0 +1,19 @@
+package javabasic;
+//Java Test
+
+public class Test {
+		public static void main(String[] args) {
+	//Test test = new Test(); //개체에 참조값을 가지고 있는 함수입니다.
+	//System.out.println(test);
+			 
+			int i = 3;
+			Integer integer =3; //wrapping 기본다입의 값을 가진 참조 타입을 생성 (첫 글자 대문자
+			int j = integer; // unwrapping 장조 타입에서 기본타입의 값을 꺼냄
+			
+			
+			
+			
+			
+		}
+
+}
